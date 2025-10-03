369f27a4b4c5fbced33adaa019253600cec0f319
eliespej <eliespej@publicisgroupe.net>
2025-10-03 10:38:46 -0300

    FIX - En los informes de Allure ya no se repiten los resultados de los test

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,8 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = { "steps", "hooks" },
-        plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" }
+        glue = { "steps", "hooks" }
 )
 public class TestRunner {
 }
